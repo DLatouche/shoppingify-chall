3b5d5bd4f336d0f5d5cc7f6723fdaa54b5f2f2f2
Dorian Latouche <dorian.latouche@hotmail.com>
2021-02-10 15:59:22 +0100

    adding annimation transition

--- src/components/containerApp/ContainerApp.jsx
+++ src/components/containerApp/ContainerApp.jsx
@@ -1,28 +1,42 @@
 import React, { memo, useCallback, useState } from "react"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { BrowserRouter, Route, Switch, useLocation } from "react-router-dom"
 import HistoryStore from "../app/history/History"
 import StatisticsStore from "../app/statistics/Statistics"
 import AsideStore from "../aside/Aside"
 import ItemsStore from "../app/items/Items"
 import MenuStore from "../menu/Menu"
 import "./ContainerApp.scss"
+import {
+    TransitionGroup,
+    CSSTransition
+} from "react-transition-group";
 
 
 const ContainerApp = () => {
-
+    let location = useLocation();
     return (
         <div className="containerApp">
             <MenuStore />
             <div className="body">
-                <Route path="/" exact>
-                    <ItemsStore />
-                </Route>
-                <Route path="/history" exact>
-                    <HistoryStore />
-                </Route>
-                <Route path="/statistics" exact>
-                    <StatisticsStore />
-                </Route>
+                <TransitionGroup>
+                    <CSSTransition
+                        key={location.key}
+                        classNames="fade"
+                        timeout={300}
+                    >
+                        <Switch location={location}>
+                            <Route path="/" exact>
+                                <ItemsStore />
+                            </Route>
+                            <Route path="/history" exact>
+                                <HistoryStore />
+                            </Route>
+                            <Route path="/statistics" exact>
+                                <StatisticsStore />
+                            </Route>
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
             </div>
             <AsideStore />
         </div>
